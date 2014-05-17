34a128facca7e6679d7847df6071f2605770f9a3
JonathanZwiebel <jonathanzwiebel@gmail.com>
2014-05-17 16:20:43 -0700

    All methods put in

--- DancefEnrir/myrObot.hpp
+++ DancefEnrir/myrObot.hpp
@@ -1,4 +1,7 @@
 #ifndef MYROBOT_H
+#define MYROBOT_H
+#include <WPILib.h>
+#include <stdlib.h>
 
 //ports for drive train victors
 #define PORT_DRIVE_VIC_LEFT_FRONT 5
@@ -9,6 +12,7 @@
 
 class RobotdEmo : public IterativeRobot
 {
+private:
 	RobotDrive myrObot;// robot drive system
 	//initialization list of Victors
 	Victor LeftvIcFront, LeftvIcbAck, RightvIcFront, RightvIcbAck;
@@ -19,8 +23,19 @@ public:
 	RobotdEmo():
 
 	void Drive();
-	//TODO including ones here
-	//initialization list that saves memory
+	void RobotInit();
+	void TurnOneWay();
+	void TurnOtherWay();
+	void RobotdEmo::DisabledInit();
+	void RobotdEmo::DisabledPeriodic();
+	void RobotdEmo::AutonomousInit();
+	void RobotdEmo::AutonomousPeriodic();
+	void RobotdEmo::TeleopInit();
+	void RobotdEmo::TeleopPeriodic();
+	void RobotdEmo::TestInit();
+	void RobotdEmo::TestPeriodic();
+	
+	
 	myrObot.SetExpiration(0.1);
 	this->SetPeriod(0); 	//Set update period to sync with robot control packets (20ms nominal)
 };
